56bf5b15d56a235016b4c2b1f6233d1a436a8d21
21-sausages <ahmedsheriif007@gmail.com>
2024-12-14 22:19:13 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let Container = document.querySelector('.Container');
+function phones(phone) {
+    img.src = phone;
+}
+function colors(color){
+    Container.style.background=color;
+}
